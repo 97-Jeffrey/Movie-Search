fcd45eb1d271b666beb23192c00a687e1ef07d33
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2022-05-09 16:10:57 -0700

    place url key and url in config.js file

--- src/App.js
+++ src/App.js
@@ -10,14 +10,17 @@ import Header from './components/header';
 // styling:
 import './App.css';
 
+import { returnUrl } from './config';
+
 function App() {
 
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
   const getMovies = async searchValue =>{
+    
     try{
-      const res =  await axios.get(`http://www.omdbapi.com/?s=${searchValue}&apikey=bdedf3e0`);
+      const res =  await axios.get(returnUrl(searchValue));
       if(res.data.Search){
         setMovies(res.data.Search)
       }
@@ -39,7 +42,6 @@ function App() {
       <div className="App">
         <Header />
         <SearchBar  
-          searchValue={searchValue}
           setSearchValue={setSearchValue}
         />
         { searchValue? 
---
+++ src/config.js
@@ -0,0 +1,2 @@
+const key = "bdedf3e0";
+export const returnUrl = searchValue => `http://www.omdbapi.com/?s=${searchValue}&apikey=${key}`;
