b2cec77a1f8b1091f2e7f687ceb72a795dd75dc2
Yu Hao Shao <yuhaoshao@Yus-MacBook-Air.local>
2024-11-20 14:35:47 -0800

    added feature of movie type searching/filtering

--- src/utils/getUrl.js
+++ src/utils/getUrl.js
@@ -1,2 +1,2 @@
 
-export const returnUrl = searchValue => `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${searchValue}`;
+export const getQueryUrl = (searchValue, searchCategory) => `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${searchValue}&type=${searchCategory}`;
